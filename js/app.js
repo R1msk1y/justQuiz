6059046de738958892cf3260cea1c02460c00e98
R1msk1y <wkurganow@gmail.com>
2022-01-25 23:45:17 +0700

    создал массив с вопрсами и ответами

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const questions = [
+
+    {
+        question: "Метод в JS, для поиска родителя элемента?",
+        answers: ["parceInt", "map", "push", "closest"],
+        aim: 4,
+    },
+
+    {
+        question: "Команда в GIT, для обращения к определенной ветке?",
+        answers: ["branch", "on", "checkout", "link"],
+        aim: 3,
+    },
+
+    {
+        question: "Команда для вывода в консоль длинны массива",
+        answers: ["typeof", "math", "length", "log"],
+        aim: 3,
+    },
+    {
+        question: "С какого значения начинаются индексы у массивов в JS?",
+        answers: ["С нуля", "С единицы", "С двух", "С минус одного"],
+        aim: 2,
+    },
+
+    {
+        question: "Какие ковычки следует использовать для написания шаблоннных строк в JS",
+        answers: ["обычные", "обратные", "одинарные", "никаких"],
+        aim: 2,
+    },
+
+
+];
+
+console.log(questions);
+
+
+
+
+const headerOfBlock = document.querySelector(`#header`);
+
